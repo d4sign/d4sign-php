6c02a61d4a026af7ceb4766491c6ce24f94dbcd8
D4Sign <rafael.figueiredo@d4sign.com.br>
2018-05-24 17:10:19 -0300

    Add files via upload

--- sdk/src/D4sign/Client.php
+++ sdk/src/D4sign/Client.php
@@ -6,6 +6,7 @@ use D4sign\Services\Safes;
 use D4sign\Services\Templates;
 use D4sign\Services\Folders;
 use D4sign\Services\Account;
+use D4sign\Services\Batches;
 
 class Client extends ClientBase
 {
@@ -18,6 +19,7 @@ class Client extends ClientBase
         $this->templates 	= new Templates($this);
         $this->folders	 	= new Folders($this);
         $this->account	 	= new Account($this);
+        $this->batches	 	= new Batches($this);
     }
     
 }
